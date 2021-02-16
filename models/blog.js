14a7d9a5b73e834a97906c0945c017c57038b1b7
muhsincaliskan <34723829+killerbean26@users.noreply.github.com>
2021-02-16 18:56:54 +0300

    part4 4.1-4.2

---
+++ models/blog.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+var uniqueValidator = require('mongoose-unique-validator')
+require('dotenv').config()
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+blogSchema.plugin(uniqueValidator)
+module.exports = mongoose.model('Blog', blogSchema)
